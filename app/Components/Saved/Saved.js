ad830a7dc327ca3da1651c9b0210c53e05b634e9
trevor bikhram <tbikhram@trevors-MacBook-Pro.local>
2017-02-22 15:15:08 -0800

    created the search component

---
+++ app/Components/Saved/Saved.js
@@ -0,0 +1,58 @@
+//include React
+var React = require("react");
+import helpers from "./../utils/helpers";
+
+class Saved extends React.Component {
+	constructor(props){
+		super(props);
+		this.deleteSavedArticle = this.deleteSavedArticle.bind(this);
+
+	}
+
+	//remove article from the database
+	deleteSavedArticle(event) {
+		let artecleIndex = parseInt(event.target.value);
+		let article = this.props.savedArticles[articleIndex];
+		//here we will make a delete request and update the state and savedArticles 
+		//only if it was succesfully removed from the database 
+		helpers.deleteArticle(articleId).then((data) => {
+			this.props.deleteArticle(articleIndex);
+
+		});
+	}
+	render(){
+		return (
+			<div id="saveSection" className="row">
+				<div className="page-header">
+					<h2>Saved Articles</h2>
+				</div>
+				<div className="panel panel-primary">
+					<div className="panel-heading">
+						<h3 className="panel-title">Articles saved on the Database</h3>
+					</div>
+					<div className="panel-body">
+					{
+						this.props.savedArcticles.map((article,articleIndex) => <div className="savedArticles col-md-12" key={article._id}>
+							<div className="row text-center">
+								<div className="col-md-4"><h3><a href={article.url}>{article.title}</a></h3></div>
+								<div className="col-md-4">Date Saved: {article.date}</div>
+								<div className="col-md-4"><button type="button" className="btn btn-danger" onClick={this.deleteSavedArticle} value={articleIndex}>Remove</button></div>
+							 </div>
+							 <div className="row">
+							 	<p className="col-md-12 text-center">Notes: Great</p>
+							 	</div>
+							 </div>
+						)
+					}
+					</div>
+				</div>
+			</div>
+
+			);
+	}
+}
+
+
+
+export default Saved;
+
---
+++ app/Components/Search/Search.js
@@ -0,0 +1,60 @@
+//include react
+
+var React = required("react");
+
+class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+
+	}
+
+	//get the values from the form and update the react state
+	handleSubmit(event){
+		event.preventDefault();
+		var beginDate = document.getElementById("startYear").value;
+		var formattedBeginDate = moment(beginDate, "MM/DD/YYYY").format("YYYYMMDD");
+		var endDate = document.getElementById("endYear").value;
+		var formattedEndDate = moment(endDate, "MM/DD/YYYY").format("YYYYMMDD");
+		var newTerm = {
+			"topic": document.getElementById("topic").value,
+			"beginDate": formattedBeginDate,
+			"endDate": formattedEndDate
+		}this.props.setTerm(newTerm);
+	}
+	render() {
+		return(
+
+			<div id="searchSection" className="row">
+				<div className="page-header">
+					<h2>Search</h2>
+				 </div>
+				 <div className="panel panel-primary">
+				     <div className="panel-heading">
+				     	<h3 className="panel-title"> Search articles on The NEW YORK TIMEs</h3>
+				     	</div>
+				     	<form className="panel-body text-center" onSubmit={this.handleSubmit}>
+				     		<div className= "panel-form-group">
+				     			<label htmlFor="topic">Topic</label>
+				     			<input type="text" className="form-control" id="topic" placeholder="Topic" />
+				     			</div>
+				     			<div className="form-group">
+				     				<label htmlFor="startYear">Start Year</label>
+				     				<input type="text" className="form-control" id="startYear" placeholder="MM/DD/YYYY" />
+				     			</div>
+				     			<div className="form-group">
+				     				<label htmlFor="endYear">End Year</label>
+				     				<input type="text" className="form-control" id="endYear" placeholder="MM/DD/YYYY" />
+				     			</div>
+				     			<button type="submit" className="btn btn-primary">Search</button>
+				     		</form>
+				     	</div>
+				     </div>	
+
+			);
+	}
+}
+
+
+
+export default Search;
